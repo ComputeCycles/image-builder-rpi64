b08e682278cb808ae61e781ad5f7263396412a69
Van Simmons <rvsrvs@gmail.com>
2019-02-09 23:15:28 -0500

    handle swift better

--- builder/chroot-script.sh
+++ builder/chroot-script.sh
@@ -267,8 +267,41 @@ sed s/debian/ubuntu/g /etc/apt/sources.list.d/swift-arm_dev-builds.list 1<> /etc
 sed s/stretch/xenial/g /etc/apt/sources.list.d/swift-arm_dev-builds.list > /etc/apt/sources.list.d/swift-arm_dev-builds.list.tmp
 mv /etc/apt/sources.list.d/swift-arm_dev-builds.list.tmp /etc/apt/sources.list.d/swift-arm_dev-builds.list
 apt-get update
-apt-get -o Dpkg::Options::="--force-overwrite" install -y --allow-unauthenticated swift5dev=5.0
+apt-get -o Dpkg::Options::="--force-overwrite" install -y --allow-unauthenticated swift5dev=6.0
+wget -q https://www.dropbox.com/s/m0vqevc7n9mcm1e/libicudata.so.55?dl=1 -O usr/lib/aarch64-linux-gnu/libicudata.so.55
+wget -q https://www.dropbox.com/s/1jm6n5bm7hti36q/libicui18n.so.55?dl=1 -O usr/lib/aarch64-linux-gnu/libicui18n.so.55
+wget -q https://www.dropbox.com/s/xptmaki14oczmos/libicuuc.so.55?dl=1 -O usr/lib/aarch64-linux-gnu/libicuuc.so.55
 
+# make llvm-6.0 the default instead 3.8, this picks up a modern lldb
+update-alternatives --install \
+        /usr/bin/llvm-config       llvm-config      /usr/bin/llvm-config-6.0  200 \
+--slave /usr/bin/llvm-ar           llvm-ar          /usr/bin/llvm-ar-6.0 \
+--slave /usr/bin/llvm-as           llvm-as          /usr/bin/llvm-as-6.0 \
+--slave /usr/bin/llvm-bcanalyzer   llvm-bcanalyzer  /usr/bin/llvm-bcanalyzer-6.0 \
+--slave /usr/bin/llvm-cov          llvm-cov         /usr/bin/llvm-cov-6.0 \
+--slave /usr/bin/llvm-diff         llvm-diff        /usr/bin/llvm-diff-6.0 \
+--slave /usr/bin/llvm-dis          llvm-dis         /usr/bin/llvm-dis-6.0 \
+--slave /usr/bin/llvm-dwarfdump    llvm-dwarfdump   /usr/bin/llvm-dwarfdump-6.0 \
+--slave /usr/bin/llvm-extract      llvm-extract     /usr/bin/llvm-extract-6.0 \
+--slave /usr/bin/llvm-link         llvm-link        /usr/bin/llvm-link-6.0 \
+--slave /usr/bin/llvm-mc           llvm-mc          /usr/bin/llvm-mc-6.0 \
+--slave /usr/bin/llvm-mcmarkup     llvm-mcmarkup    /usr/bin/llvm-mcmarkup-6.0 \
+--slave /usr/bin/llvm-nm           llvm-nm          /usr/bin/llvm-nm-6.0 \
+--slave /usr/bin/llvm-objdump      llvm-objdump     /usr/bin/llvm-objdump-6.0 \
+--slave /usr/bin/llvm-ranlib       llvm-ranlib      /usr/bin/llvm-ranlib-6.0 \
+--slave /usr/bin/llvm-readobj      llvm-readobj     /usr/bin/llvm-readobj-6.0 \
+--slave /usr/bin/llvm-rtdyld       llvm-rtdyld      /usr/bin/llvm-rtdyld-6.0 \
+--slave /usr/bin/llvm-size         llvm-size        /usr/bin/llvm-size-6.0 \
+--slave /usr/bin/llvm-stress       llvm-stress      /usr/bin/llvm-stress-6.0 \
+--slave /usr/bin/llvm-symbolizer   llvm-symbolizer  /usr/bin/llvm-symbolizer-6.0 \
+--slave /usr/bin/llvm-tblgen       llvm-tblgen      /usr/bin/llvm-tblgen-6.0
+
+update-alternatives \
+      --install /usr/bin/clang   clang   /usr/bin/clang-6.0     50 \
+      --slave   /usr/bin/clang++ clang++ /usr/bin/clang++-6.0  \
+      --slave   /usr/bin/lldb    lldb    /usr/bin/lldb-6.0 \
+      --slave   /usr/bin/lldb-server lldb-server /usr/bin/lldb-server-6.0
+      
 # cleanup APT cache and lists
 apt-get clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
